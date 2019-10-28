aa9e7c6c81526ab957bb5141993a28efef95b05b
tdm <55875354+taraDM23@users.noreply.github.com>
2019-10-28 17:18:04 +1100

    add script file, made minor html updates

---
+++ questions.js
@@ -0,0 +1,33 @@
+(function () {
+    var allQuestions = [{
+        title: "Question 1:",
+        choice: ["A", "B", "C", "D"],
+        answer: 2
+    }, {
+        title: "Question 2:",
+        choice: ["AA", "BB", "CC", "DD"],
+        answer: 3
+    }, {
+        title: "Question 3:",
+        choice: ["ping", "pong", "bong", "bing"],
+        answer: 1
+    }, {
+        title: "Question 4:",
+        choice: ["mew", "mewmew", "pew", "pew pew"],
+        answer: 0
+    }, {
+        title: "Question 5:",
+        choice: ["fa", "la", "lala", "la"],
+        answer: 1
+    }, {
+        title: "Question 6:",
+        choice: ["pew", "pewpew", "pew pew pew", "pewx4"],
+        answer: 0
+    }, {
+        title: "Question 7:",
+        choice: ["potatp", "tomato", "woof", "moo"],
+        answer: 0
+    }
+    ]
+},
+});
