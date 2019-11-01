5efb3e3b871ca8a64007f10202254ee8fb8cfc04
tdm <55875354+taraDM23@users.noreply.github.com>
2019-11-01 18:55:04 +1100

    removed function from array

--- questions.js
+++ questions.js
@@ -1,11 +1,8 @@
-(function QuesFile()
-{
-var questions = [
-    {
+var questions = [{
         title: "Question 1:",
         option: ["A", "B", "C", "D"],
         answer: 2
-    }, 
+    },
     {
         title: "Question 2:",
         option: ["AA", "BB", "CC", "DD"],
@@ -14,26 +11,26 @@ var questions = [
         title: "Question 3:",
         option: ["ping", "pong", "bong", "bing"],
         answer: 1
-    }, 
+    },
     {
         title: "Question 4:",
         option: ["mew", "mewmew", "pew", "pew pew"],
         answer: 0
-    }, 
+    },
     {
-        title:  "Question 5:",
+        title: "Question 5:",
         option: ["fa", "la", "lala", "la"],
         answer: 1
-    }, 
+    },
     {
-        title:  "Question 6:",
+        title: "Question 6:",
         option: ["pew", "pewpew", "pew pew pew", "pewx4"],
         answer: 0
-    }, 
+    },
     {
-        title:  "Question 7:",
+        title: "Question 7:",
         option: ["potatp", "tomato", "woof", "moo"],
         answer: 0
-        
+
     }
-]})
+]
