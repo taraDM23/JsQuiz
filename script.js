b04d065065dc2de23b8e003e4656c3ecedb862d3
tdm <55875354+taraDM23@users.noreply.github.com>
2019-11-02 17:25:42 +1100

    udated to script, linked score page

--- script.js
+++ script.js
@@ -10,28 +10,6 @@ const counter = document.getElementById("counter");
 const header = document.getElementById("header");
 const intro = document.getElementById("intro");
 
-var questions = [{
-    question: "Question 1: Why is this so hard:",
-    option: ["1: ping", "2: pong", "3: paddle", "4: pool"],
-    correct: 0
-}, {
-    question: "Question 2: I wish I had a lot of money",
-    option: ["1: why!!?", "2: so poor", "3: need", "4: money"],
-    correct: 3
-}, {
-    question: "Question 3: No no no no no no no",
-    option: ["1: stick", "2: to the", "3: stuff you", "4: know"],
-    correct: 1
-}, {
-    question: "Question 4: Potatos are awesome",
-    option: ["1: chips", "2: mashed", "3: wedges", "4: roast"],
-    correct: 1
-}, {
-    question: "Question 5: What are JavaScript Cookies?",
-    option: ["1: js stored in a computer when the user visits the websites to store information", "2: Cookies made with flour, eggs and HTML", "3: Chocolate", "4: Test files stored in a computer when the user visits the websites to store information"],
-    correct: 3
-}]
-
 start.addEventListener("click", startQuiz);
 
 function startQuiz() {
@@ -42,7 +20,6 @@ function startQuiz() {
     intro.style.display = "none";
     renderCounter();
     setTime();
-    //displayResult()
     TIMER = setInterval(renderCounter, 1000); // 1000ms = 1s
 }
 
@@ -79,32 +56,31 @@ function renderQuestion() {
     choiceD.innerHTML = q.option[3];
 }
 
-// counter render
+// counter 
 function renderCounter() {
+    //load 10 sec timer
     if (count <= questionTime) {
         counter.innerHTML = count;
         count++
     } else {
         count = 0;
-        // change progress color to red
-        //answerIsWrong();
+        //timeover
         if (runningQuestion < lastQuestion) {
             runningQuestion++;
             renderQuestion();
         } else {
-            // end the quiz and show the score
+            // reset fro next question
             clearInterval(TIMER);
-            scoreRender();
         }
     }
 }
 
-// checkAnwer
+// check Answer
 function checkAnswer(answer) {
     if (answer != questions[runningQuestion].correct) {
         timeleft = timeleft - 10;
     }
-    count = 0;
+    //right
     if (runningQuestion < lastQuestion) {
         runningQuestion++;
         renderQuestion();
@@ -129,42 +105,38 @@ const submit_initial = document.getElementById("submit_initial")
 const id_initial = document.getElementById("initial")
 score = document.getElementById("score")
 
-submit_initial.addEventListener("click", function(store) {
+submit_initial.addEventListener("click", function (store) {
     var store = {
         initial: id_initial.value.trim(),
         score: score.innerHTML = timeleft
     }
     console.log(store)
-})
-localStorage.setItem("store", JSON.stringify(store));
+    var storage = localStorage.getItem("store");
+    console.log(storage)
+  
+    // get stored data
+        const li = document.createElement('li');
+        li.textContent = 
+        ul.appendChild(li);
 
-// get stored data
-const ul = document.querySelector('ul');
 
-const makeli = (store) => {
-    const li = document.createElement('li');
-    li.textContent = store;
-    ul.appendChild(li);
-}
-
-itemsArray.push(input.value);
-localStorage.setItem('items', JSON.stringify(itemsArray));
-liMaker(input.value);
-input.value = "";
-
-data.forEach(item => {
-    liMaker(item);
-    console.log(store)
+    storage.push(store);
+    localStorage.setItem("store", JSON.stringify(storage));
+    console.log(localStorage.getItem("store"));
 });
+
+ 
+
+
+
 
 //Go to Home
 //location.reload()
 
-
-//view highscore
+//view highscore 
 
 /*
-Home screen  Submit button - on click event
+Home screenSubmit button - on click event
 next screens - pop up?
 screen 1 - Questions
 screen  2 - All done
